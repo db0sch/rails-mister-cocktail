eca6b5e2654c882babf61a55d2dc332fac2820f8
Dimitri Bosch <dimitribosch@hotmail.com>
2016-05-12 17:51:47 +0200

    with a bit of front

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -34,13 +34,16 @@ class DosesController < ApplicationController
   # end
 
   def destroy
-    
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+
+    redirect_to cocktail_path(params[:cocktail_id])
   end
   
   private
 
   def dose_params
-    params.require(:dose).permit(:description, :ingredient_id, :cocktail_id)
+    params.require(:dose).permit(:id, :description, :ingredient_id, :cocktail_id)
   end
 
   def find_cocktail
